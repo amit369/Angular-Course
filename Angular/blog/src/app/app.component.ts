f1c4079019314e5d2fa7e142663379932ce25fce
amit-chaman <amitchaman369@gmail.com>
2024-01-09 18:29:52 +0530

    changes done

---
+++ Angular/blog/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'blog';
+  helloworld ="hello world";
+  getValue()
+  {
+    return "Get function data"
+  }
+}
